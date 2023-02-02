13f5b5aefcf7ffd742d983842fb08b6e20f64a0b
loordbarringtn <quantas@gmail.com>
2023-02-02 14:31:29 +0300

    mod2_les2 init com

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const modalWindowTitleSelector = document.querySelector('.modal__title');
+const formSelector = document.querySelector('.modal__fieldset');
+const checkBoxOnFormSelector = document.querySelector('.modal__checkbox');
+const discountFieldSelector = document.querySelector('.modal__input_discount');
+
+console.log(modalWindowTitleSelector.attributes);
+console.log(formSelector.attributes);
+console.log(checkBoxOnFormSelector.attributes);
+console.log(discountFieldSelector.attributes);
